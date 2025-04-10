ca9c32b443d26c9f69832ded5839e543a6dbaf2d
Adam Cooper <mail@adamcooper.de>
2025-04-10 17:03:24 +0200

    downgrade to tailwind v3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,13 @@
 import type { Config } from "tailwindcss";
-import { novaTailwindTheme } from "@nova-design-system/nova-base/theme";
+import {
+  novaTailwindTheme,
+  novaTailwindPlugin,
+} from "@nova-design-system/nova-base/theme";
 
 export default {
+  content: ["./app/**/*.{html,js,ts,tsx,js,jsx}"],
   theme: novaTailwindTheme,
+  plugins: [novaTailwindPlugin],
   safelist: ["visually-hidden"],
-  darkMode: ['class', '[class="dark"'],
+  darkMode: ["class", '[class="dark"'],
 } satisfies Config;
